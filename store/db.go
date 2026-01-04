65072c08b8942331b4879c52dafb3c69ed8f97d7
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2026-01-04 18:35:39 +0545

    Implement PostgreSQL database connection function

---
+++ store/db.go
@@ -0,0 +1,24 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/LamichhaneBibek/dev-ops/config"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDb(conf *config.Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", conf.DatabaseUrl())
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+	return db, nil
+}
